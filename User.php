abb4e1aa1d9e9b48ed47c1e78f4a9d497f74e8b5
MrRapidecho <jlanglace@la-providence.net>
2022-03-08 14:44:39 +0100

    Premier User

---
+++ User.php
@@ -0,0 +1,20 @@
+﻿<?php
+echo "Chargement Class User";
+class User{
+
+    //Propriété (Private )
+    //Membres
+    private $login_;
+
+
+    //Méthode ( Public )
+    public function __construct($NewLogin){
+        $this->login_ = $NewLogin;
+    }
+
+    public function getNom(){
+        return $this->login_;
+    }
+}
+
+?>
--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-﻿<?php echo "test objet php"?>
+﻿<?php include ("User.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <h1>
+        <?php echo "Test de la class USER";
+        
+        
+        $U1 = new User("Toto");
+        $U2 = new User("Titi");
+
+        echo "<p>je suis ".$U2->getNom()."</p>";
+        echo "<p>je suis ".$U1->getNom()."</p>";
+
+        
+        ?>
+    </h1>
+</body>
+</html>
+
