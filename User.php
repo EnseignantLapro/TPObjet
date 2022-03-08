68f93d908aff2e7fa1874b64f7e68dfff46969c9
MrRapidecho <jlanglace@la-providence.net>
2022-03-08 16:00:41 +0100

    Ajout d'un formulaire

--- User.php
+++ User.php
@@ -5,16 +5,28 @@ class User{
     //Propriété (Private )
     //Membres
     private $login_;
+    private $mdp_;
 
 
     //Méthode ( Public )
-    public function __construct($NewLogin){
+    public function __construct($NewLogin,$pass){
         $this->login_ = $NewLogin;
+        $this->mdp_ = $pass;
     }
 
     public function getNom(){
         return $this->login_;
     }
-}
 
+    public function seConnecter($UnMotDePass){
+        
+        if($UnMotDePass==$this->mdp_){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+}
+highlight_file(__FILE__);
 ?>
--- index.php
+++ index.php
@@ -10,17 +10,72 @@
     <script src='main.js'></script>
 </head>
 <body>
-    <h1>
-        <?php echo "Test de la class USER";
-        
-        
-        $U1 = new User("Toto");
-        $U2 = new User("Titi");
+    
+        <?php echo "<h1>Test de la class USER</h1>"; ?>
 
-        echo "<p>je suis ".$U2->getNom()."</p>";
-        echo "<p>je suis ".$U1->getNom()."</p>";
+        <form action="" method="Post">
+            Login<input type="text" name="login">
+            mdp<input type="text" name="mdp">
+            <input type="submit" name="connexion" value="ok">
+        </form>
+
+
+        <?php
+        
+        //ici on creer plein de faux User 
+        //mais en vrai ils sont en BDD
+        $U1 = new User("toto","1234");
+        $U2 = new User("titi","abcd");
+        $U3 = new User("tata","2345");
+        $U4 = new User("tutu","34567");
+        $U5 = new User("tete","abcd");
+
+        $TableauUser = array();
+
+        array_push($TableauUser,$U1);
+        array_push($TableauUser,$U2);
+        array_push($TableauUser,$U3);
+        array_push($TableauUser,$U4);
+        array_push($TableauUser,$U5);
+       
+        if(isset($_POST["connexion"])){
+
+           //1) rechercher le bon user dans $TableauUser
+            $trouve = false;
+            foreach ($TableauUser as  $TheUser) {
+                //si le user du formulaire = le nom d'un user dans la liste alors on vérifi mdp
+                if($TheUser->getNom()==$_POST['login']){
+                    $trouve = true;
+                    //2) Vérifier le mdp
+                    //on va vérifier le mdp du formulaire avec celui de user trouvé
+                    if($TheUser->seConnecter($_POST['mdp'])){
+                        ?>
+                        <h2>Vous etes connect</h2>
+                        <?php
+                    }else{
+                        ?>
+                        <h2>Mauvais Mot de passe</h2>
+                        <?php
+                    }
+                }
+            }
+            if(!$trouve){
+                echo "User Inconnu vérifier othographe";
+            }
+
+           
+
+
+
 
         
+        }
+            
+       
+
+       
+
+        highlight_file(__FILE__);
         ?>
     </h1>
 </body>
